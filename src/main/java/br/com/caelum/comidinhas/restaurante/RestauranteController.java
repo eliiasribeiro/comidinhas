2802201d95ec36a8b758da3341c56f39a45d1b08
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2021-05-18 15:50:46 -0300

    extrai cálculo de distância para service
    co-authored-by: Elias Ribeiro <eliasctrl@live.com>

--- src/main/java/br/com/caelum/comidinhas/restaurante/RestauranteController.java
+++ src/main/java/br/com/caelum/comidinhas/restaurante/RestauranteController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static java.util.Collections.shuffle;
@@ -12,19 +13,24 @@ import static org.springframework.http.ResponseEntity.*;
 @RestController
 class RestauranteController {
 
-    private final Random random = new Random();
     private final RestauranteRepository restauranteRepository;
+    private final DistanciaService distanciaService;
 
-    RestauranteController(RestauranteRepository restauranteRepository) {
+    RestauranteController(RestauranteRepository restauranteRepository, DistanciaService distanciaService) {
         this.restauranteRepository = restauranteRepository;
+        this.distanciaService = distanciaService;
     }
 
     @GetMapping("/restaurantes-proximos/{cep:\\d{5}-\\d{3}}")
     ResponseEntity<List<RestauranteOutput>> buscaRestauranteProximos(@PathVariable("cep") String cep){
         List<Restaurante> todosOsRestaurantes = restauranteRepository.findAll();
         shuffle(todosOsRestaurantes);
-        List<RestauranteOutput> restauranteOutputs = todosOsRestaurantes.stream()
-                .map(restaurante -> new RestauranteOutput(restaurante, random.nextInt(100))).limit(5).collect(Collectors.toList());
+        List<RestauranteOutput> restauranteOutputs = todosOsRestaurantes.stream().map(restauranteOutput(cep)).limit(5)
+                .collect(Collectors.toList());
         return ok(restauranteOutputs);
+    }
+
+    private Function<Restaurante, RestauranteOutput> restauranteOutput(String cep) {
+        return restaurante -> new RestauranteOutput(restaurante, distanciaService.calculaDistancia(restaurante, cep));
     }
 }
